6a8051f749d8b0412e0a1c2a3cd6b9fad3766713
Logan D Courtney <lcourtn5@hydra23.eecs.utk.edu>
2019-03-25 10:03:31 -0400

    RIP

--- game.cpp
+++ game.cpp
@@ -6,6 +6,16 @@ const int SCREEN_HEIGHT = 480;
 
 using namespace std;
 
+//Key press surfaces constants
+enum KeyPressSurfaces {
+	KEY_PRESS_SURFACE_DEFAULT,
+	KEY_PRESS_SURFACE_UP,
+	KEY_PRESS_SURFACE_DOWN,
+	KEY_PRESS_SURFACE_LEFT,
+	KEY_PRESS_SURFACE_RIGHT,
+	KEY_PRESS_SURFACE_TOTAL
+};
+
 //Starts up SDL and creates window
 bool init();
 
@@ -25,7 +35,42 @@ SDL_Surface* gScreenSurface = NULL;
 SDL_Surface* gHelloWorld = NULL;
 
 int main(int argc, char* argv[]) {
-	
+	//Start up SDL and create window
+	if(!init()) {
+		printf("Failed to initialize!\n");
+	} else {
+		//Load media
+		if(!loadMedia()) {
+			printf("Failed to load media!\n");
+		} else {
+			//Main loop flag
+			bool quit = false;
+
+			//Event handler
+			SDL_Event e;
+
+			//While application is running
+			while(!quit) {
+				//Handle events on queue
+				while(SDL_PollEvent(&e) != 0) {
+					//User requests quit
+					if(e.type == SDL_QUIT) {
+						quit = true;
+					}
+				}
+			}
+			//Apply the image
+			SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+
+			//Update the surface
+			SDL_UpdateWindowSurface(gWindow);
+		}
+	}
+
+	//Free resources and close SDL
+	close();
+
+	return 0;
 }
 
 bool init() {
@@ -56,11 +101,24 @@ bool loadMedia() {
 	bool success = true;
 
 	//Load splash image
-	gHelloWorld = SDL_LoadBMP("preview.bmp");
-	if(gHellowWorld == NULL) {
-		printf("Unable to load image %s! SDL_Error: %s\n", "preview.bmp", SDL_GetError());
+	gHelloWorld = SDL_LoadBMP("hello_world.bmp");
+	if(gHelloWorld == NULL) {
+		printf("Unable to load image %s! SDL_Error: %s\n", "hello_world.bmp", SDL_GetError());
 		success = false;
 	}
 
 	return success;
+}
+
+void close() {
+	//Deallocate surface
+	SDL_FreeSurface(gHelloWorld);
+	gHelloWorld = NULL;
+
+	//Destroy window
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
 }
